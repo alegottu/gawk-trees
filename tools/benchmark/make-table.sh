28db1ee1411e7b0e8e840b3220c1962cd8aff90e
alegottu <alexgottuso@gmail.com>
2025-09-15 19:40:56 -0700

    add ratio column to automatically generated tables

--- tools/benchmark/make-table.sh
+++ tools/benchmark/make-table.sh
@@ -8,12 +8,12 @@ fi
 if [[ "$1" == *"-b"* ]]; then
 	shift
 	pattern='.*/[ab].*data'
-	header='| dimensions | avl | binary |
-|------------|-----|--------|'
+	header='| dimensions | avl | binary | ratio |
+|------------|-----|--------|-------|'
 else
 	pattern='.*/[^ab].*data'
-	header='| dimensions | extension | normal |
-|------------|-----------|--------|'
+	header='| dimensions | extension | normal | ratio |
+|------------|-----------|--------|-------|'
 fi;
 files=$(find "$1" -type f -regex "$pattern" -printf '%f\n')
 
@@ -27,21 +27,19 @@ done
 
 echo "$header"
 
-data=$(find -type f -regex "$pattern" -exec grep "$2" {} \; | grep -oE '[0-9].*[0-9]')
+data=$(find "$1" -type f -regex "$pattern" -exec grep "$2" {} \; | grep -oE '[0-9].*[0-9]')
 ends=""
 
 for line in $data; do
 	ends+=$(echo "| $line | ")
 done
 
-i=0
-for line in $data; do
-	n=$(expr $i / 2 + 1)
-	if [[ $(expr $i % 2) -eq 0 ]]; then
-		sed -n ${n}p <<< "$rows" | tr -d '\n'
-		echo -n "| $line "
-	else
-		echo "| $line |"
-	fi
+i=1
+while read data1; read data2
+do
+	sed -n ${i}p <<< "$rows" | tr -d '\n'
+	echo -n "| $data1 | $data2 | "
+	echo $(awk '{print $1 / $2}' <<< "$data1 $data2") \|
 	i=$(expr $i + 1)
-done
+done <<< $data
+
